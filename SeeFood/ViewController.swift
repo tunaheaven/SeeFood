6ad8f5ea46d0bad7561e4b662eaf185bd3469a34
Yoshinobu Hara <yoshihara800@gmail.com>
2021-10-22 16:27:28 -0700

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by FF2 on 10/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
